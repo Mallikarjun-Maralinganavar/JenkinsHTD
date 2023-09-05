89458835b907e34f9ff16f00cbeba9504563e616
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-05 15:13:48 +0530

    Jenkins Training

---
+++ JenkinsHTD/src/main/java/JenkinsHTD/JenkinsHTD/App.java
@@ -0,0 +1,13 @@
+package JenkinsHTD.JenkinsHTD;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
