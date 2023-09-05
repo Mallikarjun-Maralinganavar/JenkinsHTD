bfae4ed62132301be33802b16f1dc1303cdad2db
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-05 15:23:04 +0530

    Test 1

---
+++ JenkinsHTD/src/test/java/JenkinsHTD/JenkinsHTD/NewTest1.java
@@ -0,0 +1,14 @@
+package JenkinsHTD.JenkinsHTD;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class NewTest1 {
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver = new ChromeDriver();
+	  driver.get("https://www.google.com");
+  }
+}
