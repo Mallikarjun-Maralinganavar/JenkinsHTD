5f8a54bf67e1c5354f6d5a0012569db5164445e6
Mallikarjun-Maralinganavar <141898201+Mallikarjun-Maralinganavar@users.noreply.github.com>
2023-09-06 13:06:52 +0530

    Selenium

--- SeleniumJenkins/src/test/java/JenkinsHTD/SeleniumJenkins/AssertSelenium.java
+++ SeleniumJenkins/src/test/java/JenkinsHTD/SeleniumJenkins/AssertSelenium.java
@@ -1,20 +1,13 @@
 package JenkinsHTD.SeleniumJenkins;
 
-import static org.testng.Assert.assertEquals;
-
 import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.edge.EdgeDriver;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
-
-import com.beust.ah.A;
 
 public class AssertSelenium {
 	WebDriver driver;
